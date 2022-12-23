28b9981574eb9083276a04b950079836a61606cc
Nicolas Prudencio <nicolas-prudencio@hotmail.com>
2022-12-22 22:33:23 -0300

    Fix: release script

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -6,7 +6,7 @@ import {
   lineHeights,
   radii,
   space,
-} from '@ignite-ui/tokens'
+} from '@prumand-ui/tokens'
 import { createStitches, defaultThemeMap } from '@stitches/react'
 
 export const {
